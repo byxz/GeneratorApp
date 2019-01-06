fd91dce7e97a9c7b8b3047d2ca92dd61a311bd40
Evgeniy Opryshko <uncesses@gmail.com>
2019-01-06 17:24:42 +0200

    Initial Commit

---
+++ CustomObjectGenerator/ViewController.swift
@@ -0,0 +1,20 @@
+//
+//  ViewController.swift
+//  CustomObjectGenerator
+//
+//  Created by Evgeniy Opryshko on 06/01/2019.
+//  Copyright © 2019 Evgeniy Opryshko. All rights reserved.
+//
+
+import UIKit
+
+class ViewController: UIViewController {
+
+    override func viewDidLoad() {
+        super.viewDidLoad()
+        // Do any additional setup after loading the view, typically from a nib.
+    }
+
+
+}
+
