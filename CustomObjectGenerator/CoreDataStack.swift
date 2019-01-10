00d04b1f5dfa9ec0646584c4955b7761ca158d30
Evgeniy Opryshko <uncesses@gmail.com>
2019-01-10 11:35:28 +0200

    Add CoreData

---
+++ CustomObjectGenerator/CoreDataStack.swift
@@ -0,0 +1,9 @@
+//
+//  CoreDataStack.swift
+//  CustomObjectGenerator
+//
+//  Created by Evgeniy Opryshko on 09/01/2019.
+//  Copyright © 2019 Evgeniy Opryshko. All rights reserved.
+//
+
+import Foundation
---
+++ CustomObjectGenerator/RandomGenerator.swift
@@ -0,0 +1,9 @@
+//
+//  RandomGenerator.swift
+//  CustomObjectGenerator
+//
+//  Created by Evgeniy Opryshko on 06/01/2019.
+//  Copyright © 2019 Evgeniy Opryshko. All rights reserved.
+//
+
+import Foundation
